8e14903960d3058f741493aae7589e75d56e5587
Rasel Meya <55887770+raselmeya94@users.noreply.github.com>
2024-04-03 15:33:00 +0600

    Update spell_checker.py

--- spell_checker.py
+++ spell_checker.py
@@ -1,6 +1,7 @@
 import re
 import pickle
 from collections import Counter
+import Levenshtein
 
 class SpellChecker:
     def __init__(self, vocab_file, probabilities_file):
@@ -23,7 +24,7 @@ class SpellChecker:
                                'ক', 'খ', 'গ', 'ঘ', 'ঙ', 'চ', 'ছ', 'জ', 'ঝ', 'ঞ', 'ট',
                                'ঠ', 'ড', 'ঢ', 'ণ', 'ত', 'থ', 'দ', 'ধ', 'ন', 'প', 'ফ',
                                'ব', 'ভ', 'ম', 'য', 'র', 'ল', 'শ', 'ষ', 'স', 'হ', 'ড়',
-                               'ঢ়', 'য়', 'ৎ', 'ং', 'ঃ', '‍ঁ', 'ি', 'ী', 'ু', 'ূ', 'ৃ',
+                               'ঢ়', 'য়', 'ৎ', 'ং', 'ঃ', '‍ঁ', 'া' , 'ি', 'ী', 'ু', 'ূ', 'ৃ',
                                'ৄ', 'ে', 'ৈ', 'ো', 'ৌ', '্']
         return [word[:i] + char + word[i + 1:] for i in range(len(word)) for char in bengali_characters if
                 char != word[i]]
@@ -33,7 +34,7 @@ class SpellChecker:
                                'ক', 'খ', 'গ', 'ঘ', 'ঙ', 'চ', 'ছ', 'জ', 'ঝ', 'ঞ', 'ট',
                                'ঠ', 'ড', 'ঢ', 'ণ', 'ত', 'থ', 'দ', 'ধ', 'ন', 'প', 'ফ',
                                'ব', 'ভ', 'ম', 'য', 'র', 'ল', 'শ', 'ষ', 'স', 'হ', 'ড়',
-                               'ঢ়', 'য়', 'ৎ', 'ং', 'ঃ', '‍ঁ', 'ি', 'ী', 'ু', 'ূ', 'ৃ',
+                               'ঢ়', 'য়', 'ৎ', 'ং', 'ঃ', '‍ঁ', 'া' ,  'ি', 'ী', 'ু', 'ূ', 'ৃ',
                                'ৄ', 'ে', 'ৈ', 'ো', 'ৌ', '্']
         return [word[:i] + char + word[i:] for i in range(len(word) + 1) for char in bengali_characters]
 
@@ -43,17 +44,54 @@ class SpellChecker:
 
     def level_two_edit(self, word):
         return set(word2 for word1 in self.level_one_edit(word) for word2 in self.level_one_edit(word1))
+    
+    import Levenshtein
 
-    def correct_word(self, word):
-        if word in self.vocab:
-            return word
-        suggestions = self.level_one_edit(word) or self.level_two_edit(word) or [word]
-        best_guesses = [word for word in suggestions if word in self.vocab]
+    def calculate_text_distance(self, text1, text2):
+        distance = Levenshtein.distance(text1, text2)
+        return distance
+
+    def word_similarity_checker(self, misspelled_word, predicted_word=None):
+        bengali_characters = ['অ', 'আ', 'ই', 'ঈ', 'উ', 'ঊ', 'ঋ', 'এ', 'ঐ', 'ও', 'ঔ',
+                            'ক', 'খ', 'গ', 'ঘ', 'ঙ', 'চ', 'ছ', 'জ', 'ঝ', 'ঞ', 'ট',
+                            'ঠ', 'ড', 'ঢ', 'ণ', 'ত', 'থ', 'দ', 'ধ', 'ন', 'প', 'ফ',
+                            'ব', 'ভ', 'ম', 'য', 'র', 'ল', 'শ', 'ষ', 'স', 'হ', 'ড়',
+                            'ঢ়', 'য়', 'ৎ']
+        misspelled_chars= [chac_ for chac_ in (list(misspelled_word)) if chac_ in bengali_characters]
+        misspelled_chars.sort()
+        misspelled_chars="".join(misspelled_chars)
+        if predicted_word is None:
+            return misspelled_chars
+            
+        predicted_chars= [chac_ for chac_ in (list(predicted_word)) if chac_ in bengali_characters]
+        predicted_chars.sort()
+        predicted_chars="".join(predicted_chars)
+        distance=self.calculate_text_distance(misspelled_chars, predicted_chars )
+        if distance in [0,1]:
+            return predicted_chars
+        else:
+            return ""
+    def best_words(self, misspelled_word):
+        
+        suggestions=  self.level_one_edit(misspelled_word) and self.level_two_edit(misspelled_word) or [misspelled_word]
+
+        best_guesses=[word for word in suggestions if word in self.vocab]
+
+        word_len=len(self.word_similarity_checker(misspelled_word))
+        return [(word , self.word_probabilities[word]) for word in best_guesses if word_len<=len(self.word_similarity_checker(misspelled_word , word)) ]
+        
+
+    def correct_word(self, misspelled_word):
+        if misspelled_word in self.vocab:
+            return misspelled_word
+        
+        best_guesses = self.best_words(misspelled_word)
+        # print(best_guesses, sep=" \n")
         if best_guesses:
-            return max(best_guesses, key=lambda x: self.word_probabilities[x])
-        return word
+            return max(best_guesses, key=lambda x: x[1], default=('', 0))[0]
+        return misspelled_word
 
     def correct_spelling(self, text):
-        words = text.split()
-        corrected_words = [self.correct_word(word) for word in words]
-        return ' '.join(corrected_words)
+
+        corrected_words = self.correct_word(text)
+        return corrected_words
